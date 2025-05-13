f16a602d864ae92ac703b28fd0abe54c034793d6
neomichael <talentmichael123@gmail.com>
2025-05-13 20:32:33 +0800

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './index.css'
-import Quiz from './components/quiz';
+import React from "react";
+import { QuizProvider } from "./context/quiz-context";
+import Quiz from "./components/quiz";
 
 function App() {
   return (
-    <div className='app-container'>
-      <h1>Quiz App</h1>
-      <Quiz />
-    </div>
+    <QuizProvider>
+      <div className="app-container">
+        <h1>Quiz App</h1>
+        <Quiz />
+      </div>
+    </QuizProvider>
   );
 }
 
